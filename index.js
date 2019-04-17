9df6143d958950c310ffa93b7af05f863ba8aeec
oicramkroll <oicarmkroll@gmail.com>
2019-04-17 12:47:42 -0300

    finalizar primeira etapa

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const readline = require('readline-sync');
 function start(){
     const content = {};
     content.searchTerm = askReturnSearchTerm();
+    content.askPrefeix = askReturnPrefix();
     function askReturnSearchTerm(){
-        return readline.question("qual o termo de busca?");
+        return readline.question("qual o termo? ");
+    }
+    function askReturnPrefix(){
+        const prefixes = ["quem e?","o que e?","a historia de."];
+        const selectedPrefix = readline.keyInSelect(prefixes,"qual o prefixo");
+        return prefixes[selectedPrefix];
     }
     console.log(content);
 }
